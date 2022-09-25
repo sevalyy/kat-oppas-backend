0b2d2a0dfa9cf5d23fba118c7825c3ea5c74abd7
sevalyy <seval@yamanyar.com>
2022-09-25 21:19:48 +0200

    Use chained calls on app

--- index.js
+++ index.js
@@ -15,16 +15,19 @@ const app = express();
 // CORS middleware:  * Since our api is hosted on a different domain than our client
 // we are are doing "Cross Origin Resource Sharing" (cors)
 // Cross origin resource sharing is disabled by express by default
-app.use(corsMiddleWare());
+app
+  .use(corsMiddleWare())
 
-// express.json() to be able to read request bodies of JSON requests a.k.a. body-parser
-app.use(express.json());
+  // express.json() to be able to read request bodies of JSON requests a.k.a. body-parser
+  .use(express.json())
 
-//routes
-app.use("/auth", authRouter);
-app.use("/reservations", reservationRouter);
+  //routes
+  .use("/auth", authRouter)
+  .use("/reservations", reservationRouter)
+
+  //start listening
+  .listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+
 initScheduledJobs();
-//start listening
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
